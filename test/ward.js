c4fc3c56a1db0707bbc91bf92d84c5e073188ecf
boo1ean <boo1ean0807@gmail.com>
2014-01-15 23:41:54 +0200

    Add tests for ward

---
+++ test/ward.js
@@ -0,0 +1,35 @@
+var createWard = require('../lib/ward'),
+    should = require('should'),
+    fs = require('fs'),
+    exists = fs.existsSync,
+    read = fs.readFileSync,
+    unlink = fs.unlinkSync;
+
+
+describe('File storage ward', function() {
+	it('Should be ward factory function', function() {
+		createWard.should.be.type('function');
+		createWard('test').should.be.type('function');
+	});
+
+	it('Should create file at given path with empty json array if it does not exist and call next', function(done) {
+		var path = __dirname + '/assets/test';
+
+		createWard(path)(function() {
+			exists(path).should.be.ok;
+			JSON.parse(read(path)).should.be.instanceof(Array).and.have.lengthOf(0);
+			unlink(path);
+			done();
+		})
+	});
+
+	it('Should not affect file if it already exists', function(done) {
+		var path = __dirname + '/assets/.migrations';
+		var expectedContent = 'wow such content\n';
+
+		createWard(path)(function() {
+			read(path).toString().should.be.exactly(expectedContent);
+			done();
+		});
+	});
+});
